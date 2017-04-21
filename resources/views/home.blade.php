b7015d32d8f38f9c78b15243c0cb8bdde9b79ce1
emil.cerezo <emilio.cerezo@republisys.com>
2017-04-21 11:12:02 +0800

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <aside class="sideContainer">
+        Emil Cerezo
+    </aside>
+
+    <div class="chatContainer">
+
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html lang="{{ config('app.locale') }}">
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+        <title>iPost - @yield('title')</title>
+
+        <script>
+
+            window.Laravel = <?php echo json_encode([
+                'csrfToken' => csrf_token(),
+             ]); ?>
+
+        </script>
+
+        <script src="{{ asset('/js/jquery.js') }}"></script>
+
+        <script src="{{ asset('/js/app.js') }}"></script>
+
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+
+        <script src="{{ asset('/js/socket.io.js') }}"></script>
+
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+
+    </body>
+
+</html>
